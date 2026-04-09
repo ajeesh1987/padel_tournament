5adc0409bd4c042c6f81fe7a40a655e7048bc05f
Ajeesh Damodaran <77533782+ajeesh1987@users.noreply.github.com>
2026-04-09 14:10:21 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './rules.tsx'
+// Remove the '.tsx' extension here
+import App from './rules' 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
